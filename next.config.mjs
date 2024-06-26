d9067c6289e757075e7a1f10294a3e280837bdbe
Dmitry Kutsevalov <kutdmitry@gmail.com>
2024-06-26 18:20:24 +0300

    4.4th commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.resolve(__dirname, "./"),
+    };
+    return config;
+  },
   experimental: {
     ppr: "incremental",
   },
 };
-
 export default nextConfig;
